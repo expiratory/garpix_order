9c8b242b8a9d3420cdf2d7979c8e34f14d515d59
anatolijburenkov <worms102@yandex.ru>
2021-11-04 22:02:11 +0300

    add setup

--- backend/garpix_order/apps.py
+++ backend/garpix_order/apps.py
@@ -2,6 +2,6 @@ from django.apps import AppConfig
 from django.utils.translation import ugettext_lazy as _
 
 
-class OrderConfig(AppConfig):
+class GarpixOrderConfig(AppConfig):
     name = 'garpix_order'
     verbose_name = _('Order')
