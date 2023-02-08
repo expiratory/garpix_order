f5262156b4389f08eea47d0851f42d872694372b
AnatolyBur <worm102@yandex.ru>
2023-02-08 16:12:06 +0300

    новое view и проверка на hmac_sha256

--- backend/garpix_order/urls.py
+++ backend/garpix_order/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path
-from .views.cloudpayments.pay import pay_view
-from .views.cloudpayments.fail import fail_view
-from .views.cloudpayments.payment_data import payment_data_view
+from .views.cloudpayments import CloudpaymentView
 
 urlpatterns = [
-    path('cloudpayments/pay/', pay_view),
-    path('cloudpayments/fail/', fail_view),
-    path('cloudpayments/payment_data/', payment_data_view),
+    path('cloudpayments/pay/', CloudpaymentView.pay_view),
+    path('cloudpayments/fail/', CloudpaymentView.fail_view),
+    path('cloudpayments/payment_data/', CloudpaymentView.payment_data_view),
 ]
